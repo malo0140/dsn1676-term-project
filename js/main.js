9b4f5310bf2f991e1c01650a2122d7535d3f2abb
malo0140 <karamaloney@Karas-MacBook-Pro.local>
2015-03-20 18:47:06 -0400

    images added and js working

--- js/main.js
+++ js/main.js
@@ -1,13 +1,28 @@
 var $btnCollapse = $('.collapse');
 var $panel = $('.panel');
 
-var $bouncebtn = $('.bounce-btn');
-var $card = $('.cardguy');
+var $btnFade = $('.fade');
+var $quote = $('.quote');
+
+var $BtnFadeIn = $('.fadein');
+var $late = $('.late');
+
+var $btnFadeText = $('.fadetext');
+var $knob = $('.knob');
 
 $btnCollapse.on('click', function () {
     $panel.toggleClass('js-panel');
 });
 
-$bouncebtn.on('click', function () {
-    $card.toggleClass('js-card');
+$btnFade.on('click', function () {
+    $quote.toggleClass('js-fade');
+});
+
+$BtnFadeIn.on('click', function () {
+    $late.toggleClass('js-fadein');
+});
+
+$btnFadeText.on('click', function () {
+    $knob.toggleClass('js-fadetext');
+    
 });

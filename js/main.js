01801e384b4f6bf3ce0c499ff2ea7eb0c4e35516
malo0140 <karamaloney@Karas-MacBook-Pro.local>
2015-04-25 11:48:43 -0400

    offset changed

--- js/main.js
+++ js/main.js
@@ -45,5 +45,5 @@ var $bottleInwater = $('.bottle-in-water');
 
 $bottleInwater.waypoint(function () {
     $bottleInwater.toggleClass('js-dissapear');
-    }, {offset: '20%'});
+    }, {offset: '50%'});
 
